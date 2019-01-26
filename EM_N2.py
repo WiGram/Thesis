7642b4c6e8335a876e26ae05d6f1f224185a034b
WiGram <william.gram@outlook.com>
2019-01-26 12:41:07 +0100

    fix jit

--- EM_N2.py
+++ EM_N2.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 import quandl
 import scipy.optimize as opt
+from numba import jit
 from pandas_datareader import data as web
 np.set_printoptions(suppress = True)   # Disable scientific notation
 
@@ -19,20 +20,23 @@ np.set_printoptions(suppress = True)   # Disable scientific notation
 # ============================================= #
 
 # Output is f1, f2, ..., fN; var, mu must be arrays of parameters (i.e. for each state)
+@jit
 def muFct(pStar, returns, states):
-    # mu = [sum(pStar[s, :] * returns[i,:]) / sum(pStar[s, :]) for s in range(states) for i in range(len(returns[:,0]))] # further inspection
-    mu1 = [sum(pStar[s, :] * returns[0,:]) / sum(pStar[s, :]) for s in range(states)]
-    mu2 = [sum(pStar[s, :] * returns[1,:]) / sum(pStar[s, :]) for s in range(states)]
+    # mu = [np.sum(pStar[s, :] * returns[i,:]) / np.sum(pStar[s, :]) for s in range(states) for i in range(len(returns[:,0]))] # further inspection
+    mu1 = [np.sum(pStar[s, :] * returns[0,:]) / np.sum(pStar[s, :]) for s in range(states)]
+    mu2 = [np.sum(pStar[s, :] * returns[1,:]) / np.sum(pStar[s, :]) for s in range(states)]
     return np.array([mu1, mu2])
 
 # Output: v1^2, v2^2, ..., vN^2
+@jit
 def varFct(pStar, returns, mu, states):
-    var1 = [sum(pStar[s, :] * (returns[0,:] - mu[0,s]) ** 2) / sum(pStar[s, :]) for s in range(states)]
-    var2 = [sum(pStar[s, :] * (returns[1,:] - mu[1,s]) ** 2) / sum(pStar[s, :]) for s in range(states)]
-    cov  = [sum(pStar[s, :] * (returns[0,:] - mu[0,s]) * (returns[1,:] - mu[1,s])) / sum(pStar[s, :]) for s in range(states)]
+    var1 = [np.sum(pStar[s, :] * (returns[0,:] - mu[0,s]) ** 2) / np.sum(pStar[s, :]) for s in range(states)]
+    var2 = [np.sum(pStar[s, :] * (returns[1,:] - mu[1,s]) ** 2) / np.sum(pStar[s, :]) for s in range(states)]
+    cov  = [np.sum(pStar[s, :] * (returns[0,:] - mu[0,s]) * (returns[1,:] - mu[1,s])) / np.sum(pStar[s, :]) for s in range(states)]
     covm = [ np.array([[var1[s],cov[s]], [cov[s],var2[s]]]) for s in range(states)]
     return np.array(covm)
 
+@jit
 def fFct(returns, mu, covm, states):
     det             = np.linalg.det(covm) # returns [det1, det2, ..., detN]
     demeanedReturns = np.array([ np.array([ returns[0,:] - mu[0,s], 
@@ -41,13 +45,15 @@ def fFct(returns, mu, covm, states):
     return np.array(f)
 
 # Output: p11, p12, ..., p1N, p21, p22, ..., p2N, ..., pN1, pN2, ..., pNN
+@jit
 def pFct(pStarT, states):
     n   = states
-    den = [sum([sum(pStarT[s * n + i,:]) for i in range(states)]) for s in range(states)]
-    p   = [sum(pStarT[s * n + i,:]) / den[s] for s in range(states) for i in range(states)]
+    den = [np.sum([np.sum(pStarT[s * n + i,:]) for i in range(states)]) for s in range(states)]
+    p   = [np.sum(pStarT[s * n + i,:]) / den[s] for s in range(states) for i in range(states)]
     return np.array(p)
 
 # A. Forward algorithm
+@jit
 def aFct(mat, states, f, p):
     a   = [f[i][0] / states for i in range(states)]  # v_j = 1/N, N = states.
     a   = np.repeat(a, mat).reshape(states, mat)
@@ -55,56 +61,62 @@ def aFct(mat, states, f, p):
     a_r = np.ones(states * mat).reshape(states, mat) # a_rescale
 
     # t = 0
-    a_s[0]    = sum(a[:,0])
+    a_s[0]    = np.sum(a[:,0])
     a_r[:, 0] = a[:,0] / a_s[0]
 
     # t in [1, T]
     for t in range(1, mat):
-        a[:, t]   = [f[t,s] * sum([p[S * states + s] * a_r[S, t-1] for S in range(states)]) for s in range(states)]
-        a_s[t]    = sum(a[:, t])
+        a[:, t]   = [f[t,s] * np.sum([p[S * states + s] * a_r[S, t-1] for S in range(states)]) for s in range(states)]
+        a_s[t]    = np.sum(a[:, t])
         a_r[:, t] = a[:,t] / a_s[t]
 
     return np.array(a_r), np.array(a_s)
 
 # B. Backward algorithm
+@jit
 def bFct(mat, states, f, p):    
     b   = np.ones(states * mat).reshape(states, mat)
     b_s = np.ones(mat)                               # b_scale
     b_r = np.ones(states * mat).reshape(states, mat) # b_rescale
 
     # t = T (= mat - 1)
-    b_s[mat-1]      = sum(b[:, mat - 1])
+    b_s[mat-1]      = np.sum(b[:, mat - 1])
     b_r[:, mat - 1] = b[:, mat - 1] / b_s[mat - 1]
 
     # t in [0, T - 1] (= mat - 2, stops at previous index, i.e. 0)
     for t in range(mat - 2, -1, -1):
-        b[:, t]   = [sum([b_r[S, t+1] * f[t+1,S] * p[s * states + S] for S in range(states)]) for s in range(states)]
-        b_s[t]    = sum(b[:,t])
+        b[:, t]   = [np.sum([b_r[S, t+1] * f[t+1,S] * p[s * states + S] for S in range(states)]) for s in range(states)]
+        b_s[t]    = np.sum(b[:,t])
         b_r[:, t] = b[:, t] / b_s[t]
 
     return np.array(b_r)
 
 # Output (smoothed) p1, p2, ..., pN
+@jit
 def pStarFct(mat, states, a_r, b_r):
-    den   = sum([b_r[s, :] * a_r[s, :] for s in range(states)])
+    den   = np.sum([b_r[s, :] * a_r[s, :] for s in range(states)])
     pStar = [b_r[s, :] * a_r[s,:] / den for s in range(states)]
     return np.array(pStar)
 
 # Output (smoothed transition) p11, p12, ..., p1N, p21, p22, ..., p2N, pN1, pN2, ..., pNN
-def pStarTFct(mat, states, a_r, a_s, b_r, p):
+@jit
+def pStarTFct(f, mat, states, a_r, a_s, b_r, p):
     pStarT = np.ones(states * states * mat).reshape(states * states, mat)
 
-    den   = sum([b_r[s, :] * a_r[s, :] for s in range(states)]) * a_s
+    den   = np.sum([b_r[s, :] * a_r[s, :] for s in range(states)]) * a_s
     pStarT[:, 0] = p / states
     pStarT[:, 1:] = [b_r[s, 1:] * f[1:,s] * p[S * states + s] * a_r[S, :mat - 1] / den[1:] for S in range(states) for s in range(states)]
     return np.array(pStarT)
 
 # E. Expected log-likelihood function to maximise
-def logLikFct(mu, var, p, pStar, pStarT):
+@jit
+def logLikFct(returns, mu, covm, p, pStar, pStarT):
+    f = fFct(returns, mu, covm, states)
     k = -0.5 * (np.log(2 * np.pi) + 1.0)  # the constant 'c' is set to 1.0
-    a = sum([sum([np.log(p[s * states + S]) * sum(pStarT[s * states + S, 1:]) for S in range(states)]) for s in range(states)])
-    b = sum([-0.5 * sum(pStar[s, :] * (np.log(var[s]) + (y -mu[s]) ** 2 / var[s])) for s in range(states)])
+    a = np.sum([np.sum([np.log(p[s * states + S]) * np.sum(pStarT[s * states + S, 1:]) for S in range(states)]) for s in range(states)])
+    b = np.sum([-0.5 * np.sum(pStar[s, :] * f[:, s]) for s in range(states)])
     return k + a + b
+
 
 # ============================================= #
 # ===== Start running the programme =========== #
@@ -123,16 +135,18 @@ src = 'yahoo'
 # Returns apple first, S&P second..
 close = web.DataReader(sbl, src, bgn, end)['Close']
 # sp500 = web.DataReader(sbl, src, bgn, end)['Close'].to_frame().resample('MS').mean().round() # Monthly
-prices = np.array(close)
-d     = np.array(close.index, dtype = 'datetime64[D]')[1:]
-mat   = len(prices[:,0])
-y     = np.array([(np.log(prices[1:,i]) - np.log(prices[:mat-1,i])) * 100 for i in range(len(sbl))])
+prices  = np.array(close)
+d       = np.array(close.index, dtype = 'datetime64[D]')[1:]
+mat     = len(prices[:,0])
+returns = np.array([(np.log(prices[1:,i]) - np.log(prices[:mat-1,i])) * 100 for i in range(len(sbl))])
 
-returns = y # will need for generating the dynamic function later..
-
+sims     = 50
 states   = 3
 assets   = len(sbl)
-sims     = 500
+
+# ============================================= #
+# ===== EM Algorithm ========================== #
+# ============================================= #
 
 # 1. Set initial parameters
 # def runEstimation(returns, sims, states):
@@ -151,13 +165,13 @@ mu  = muFct(pStarRandom, returns, states)
 var = varFct(pStarRandom, returns, mu, states)
 p   = np.repeat(1.0 / states, states * states)
 
-f       = fFct(returns, mu, var, states)
+f   = fFct(returns, mu, var, states)
 
 a_r, a_s = aFct(mat, states, f, p)
 b_r      = bFct(mat, states, f, p)
 
 pStar    = pStarFct(mat, states, a_r, b_r)
-pStarT   = pStarTFct(mat, states, a_r, a_s, b_r, p)
+pStarT   = pStarTFct(f, mat, states, a_r, a_s, b_r, p)
 
 # 3. EM-loop until convergence (we loop sims amount of times)
 for m in range(sims):
@@ -172,10 +186,10 @@ for m in range(sims):
     b_r = bFct(mat, states, f, p)
 
     pStar  = pStarFct(mat, states, a_r, b_r)
-    pStarT = pStarTFct(mat, states, a_r, a_s, b_r, p)
+    pStarT = pStarTFct(f, mat, states, a_r, a_s, b_r, p)
     
     # Compute the log-likelihood to maximise
-    logLik = logLikFct(mu, var, p, pStar, pStarT)
+    logLik = logLikFct(returns, mu, var, p, pStar, pStarT)
 
     # Save parameters for later plotting (redundant wrt optimisation)
     ms[m]  = mu
@@ -183,7 +197,6 @@ for m in range(sims):
     ps[m]  = p.reshape(3,3)
     llh[m] = logLik
 
-    # return np.array(vs, ms, ps, llh, pStar, pStarT)
 
 # test = runEstimation(y, sims, states)
 
@@ -196,9 +209,12 @@ if states == 2:
     pltm.plotDuo(range(sims), ms[0,:], ms[1,:], 'Mu_1', 'Mu_2', 'Trials', 'Mean return')
     pltm.plotDuo(range(sims), ps[0,:], ps[3,:], 'p11', 'p22', 'Trials', 'Probability')
 elif states == 3:
-    pltm.plotTri(range(sims), vs[0,:], vs[1,:], vs[2,:], 'Trials', 'Var_1', 'Var_2', 'Var_3', 'Variance')
-    pltm.plotTri(range(sims), ms[0,:], ms[1,:], ms[2,:], 'Trials', 'Mu_1', 'Mu_2', 'Mu_3', 'Mean return')
-    pltm.plotTri(range(sims), ps[0,:], ps[4,:], ps[8,:], 'Trials', 'p11', 'p22', 'p33', 'Probability')
+    # vs: vs[m, s, asset, asset] -> covariance for vs[m, s, 0, 1] or vs[m, s, 1, 0]
+    pltm.plotTri(range(sims), vs[:,0,0,0], vs[:, 1, 0, 0], vs[:, 2, 0, 0], 'Trials', 'Var_1', 'Var_2', 'Var_3', 'Variance', title = ('Variance of %s' % sbl[1]))
+    pltm.plotTri(range(sims), vs[:,0,1,1], vs[:, 1, 1, 1], vs[:, 2, 1, 1], 'Trials', 'Var_1', 'Var_2', 'Var_3', 'Variance', title = ('Variance of %s' % sbl[0]))
+    pltm.plotTri(range(sims), ms[:, 0, 0], ms[:, 0, 1], ms[:, 0, 2], 'Trials', 'Mu_1', 'Mu_2', 'Mu_3', 'Mean return', title = ('Mean of %s' % sbl[1]))
+    pltm.plotTri(range(sims), ms[:, 1, 0], ms[:, 1, 1], ms[:, 1, 2], 'Trials', 'Mu_1', 'Mu_2', 'Mu_3', 'Mean return', title = ('Mean of %s' % sbl[0]))
+    pltm.plotTri(range(sims), ps[:,0,0], ps[:, 1, 1], ps[:, 2, 2], 'Trials', 'p11', 'p22', 'p33', 'Probability')
     pltm.plotUno(d, pStar[0,:], xLab = 'Time', yLab = 'p1', title = 'Smoothed State Probabilities')
     pltm.plotUno(d, pStar[1,:], xLab = 'Time', yLab = 'p2', title = 'Smoothed State Probabilities')
     pltm.plotUno(d, pStar[2,:], xLab = 'Time', yLab = 'p3', title = 'Smoothed State Probabilities')
