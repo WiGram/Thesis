8dc13c0be5d6a0bb5cd02f3a27f344925b87102e
WiGram <william.gram@outlook.com>
2019-02-10 14:25:07 +0100

    mpt analysis

--- descriptive.py
+++ descriptive.py
@@ -9,64 +9,111 @@ monthly Bloomberg data. Time series are illustrated
 and the first two moments are estimated.
 """
 
-import likelihoodModule as llm
-import plotsModule as pltm
+import markowitzOpt as mpt
 import numpy as np
 import pandas as pd
 import quandl
 import scipy.optimize as opt
+from scipy.optimize import minimize
 from matplotlib import pyplot as plt
 # import matplotlib.style as style
 from numba import jit
 from pandas_datareader import data as web
-# import seaborn as sns
 plt.rcParams.update(plt.rcParamsDefault)
 plt.style.use('seaborn-paper')
 np.set_printoptions(suppress = True)   # Disable scientific notation
 
+# Read data into a Pandas data frame
 bbData = pd.read_csv('/home/william/Dropbox/Thesis/mthReturns.csv', index_col=0,header=0)
-bbData.index = pd.to_datetime(bbData.index, format = '%Y-%m-%d')
+
+# Set format of index to a date format
+bbData.index = pd.to_datetime(bbData.index)
+
+# Sort data with oldest data first
 bbData = bbData.sort_index()
+
+# Extract column names
 colNames = list(bbData)
-rows = len(bbData[colNames[0]])
+
+# Count amount of assets
+assets = len(colNames)
+
+# Define a vector d, to be used on the x-axis of plots
 d = bbData.index
 
+# Plot index price series
 bbData.iloc[:,:len(colNames)].plot()
 plt.show()
 
+# Hard coded return and volatility column names
 retList = ['HY_ret','IG_ret','CD_ret','R2_ret','R1_ret','SP_ret']
 
+# Generate return time series for each index
 for i in range(len(retList)):
     bbData[retList[i]] = bbData[colNames[i]].pct_change()
 
-# 321: Plot rows = 3, plot columns = 2, plot is no. 1
-plotPos = [321, 322, 323, 324, 325, 326]
-
-for i in range(len(plotPos)):
-    plt.subplot(plotPos[i])
-    plt.plot(d, bbData[colNames[i]])
-plt.subplots_adjust(top = 0.92, bottom = 0.08, left = 0.10, right = 0.95,
-                    hspace = 0.5, wspace = 0.35)
+# Generate return correlation plots
+import seaborn as sns
+sns.pairplot(bbData.iloc[:,assets:2*assets])
 plt.show()
 
+# ============================================= #
+# ===== Analysis of returns =================== #
+# ============================================= #
 
+# Applying log returns definition
+monthlyRets = np.log(bbData.iloc[:,:assets]/bbData.iloc[:,:assets].shift(1))
 
-for i in range(len(plotPos)):
-    plt.subplot(plotPos[i])
-    plt.plot(d, bbData[retList[i]])
+# Histogram of mean returns
+monthlyRets.hist(bins=100,figsize=(12,6))
 plt.show()
 
-descriptive = bbData.describe()
-
-# With pandas: use iloc for slicing by indices
-retDes = descriptive.iloc[:,len(colNames):]
-
-vols  = retDes.iloc[2,:]
-means = retDes.iloc[1,:]
-SR = means / vols
-
-SR.plot.bar(y = SR)
-plt.bar(SR, 1)
+# Time series plots of each return process
+monthlyRets.plot(subplots = True, layout = (int(assets / 2), 2), figsize = (8,16))
 plt.show()
 
-sns.pairplot(bbData.iloc[:,len(colNames):])
+# Moments and quartiles of return processes
+monthlyRets.describe().transpose()
+
+# ============================================= #
+# ===== Analysis of volatility ================ #
+# ============================================= #
+
+# Var is squared return process, assuming true mean return of 0
+monthlyVol = np.sqrt(monthlyRets ** 2)
+
+# Time series plot of each squared return process
+monthlyVol.plot(subplots = True, layout = (int(assets / 2), 2), figsize = (8,16))
+plt.show()
+
+# monthlyRets.cov() * 12 # Yearly covariance matrix
+retCov = monthlyRets.cov()
+
+# ============================================= #
+# ===== Analysis of autocorrelation =========== #
+# ============================================= #
+
+def label(ax, string):
+    ax.annotate(string, (1, 1), xytext=(-8, -8), ha='right', va='top',
+                size=14, xycoords='axes fraction', textcoords='offset points')
+
+fig, axes = plt.subplots(nrows=assets, figsize=(8, 12))
+fig.tight_layout()
+
+# Der antydes her en fejl. Dette ser ud til at være en falsk positiv - der er ingen fejl
+for i in range(assets):
+    pd.tools.plotting.autocorrelation_plot(monthlyRets.iloc[1:21,0], ax = axes[i])
+    label(axes[i], colNames[i])
+plt.show()
+
+# ============================================= #
+# ===== Analysis of optimal MPT portfolio ===== #
+# ============================================= #
+
+sims     = 50000
+mptOutput = mpt.mptPortfolios(sims, monthlyRets, assets)
+mpt.mptScatter(mptOutput['pfVol'], mptOutput['pfRet'],mptOutput['pfSR'],mptOutput['weights'],monthlyRets, n = 12)
+
+"""
+The end
+"""
---
+++ markowitzOpt.py
@@ -0,0 +1,132 @@
+"""
+Date:    February 10th, 2019
+Authors: Kristian Strand and William Gram
+Subject: Markowitz portfolio theory (MPF)
+
+Description:
+This script implements an algorithm to find
+the optimal portfolio allocation through
+Markowitz Portfolio Theory, i.e. optimising
+the Sharpe ratio.
+"""
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from scipy.optimize import minimize
+
+
+# Technicality on plotting layout
+plt.rcParams.update(plt.rcParamsDefault)
+plt.style.use('seaborn-paper')
+np.set_printoptions(suppress = True)   # Disable scientific notation
+
+# Optimisation functions
+def neg_sharpe(weights, returns, n):
+    return  get_ret_vol_sr(weights, returns, n)[2] * -1
+
+# Contraints
+def check_sum(weights):
+    '''
+    Returns 0 if sum of weights is 1.0
+    '''
+    return np.sum(weights) - 1
+
+# Alternative approach to finding optimal weights
+def get_ret_vol_sr(weights, returns, n):
+    """
+    Takes in weights, returns array or return,volatility, sharpe ratio
+
+    1. Be observant as to "returns" being well-defined
+    2. Be observant as to "n" being well-defined
+    """
+    weights = np.array(weights)
+    ret = np.sum(returns.mean() * weights) * n
+    vol = np.sqrt(np.dot(weights.T, np.dot(returns.cov() * n, weights)))
+    sr = ret/vol
+    return np.array([ret,vol,sr])
+
+def mptPortfolios(sims, returns, assets, frequency = 'm'):
+    # Frequency must be either days or months
+    if frequency == 'm':
+        n = 12
+    elif frequency == 'd':
+        n = 252
+    else:
+        raise Exception("Use 'd' for 'days' or 'm' for 'months'")
+    
+    retMeans = returns.mean()
+    retCov   = returns.cov()
+
+    weights  = np.random.random(size = (assets, sims))
+    colSum   = np.sum(weights, axis = 0)
+    weights /= colSum
+
+    pfRet = np.array([np.sum(retMeans * weights[:,i]) * n for i in range(sims)])
+    pfVol = np.array([np.sqrt(np.dot(weights[:,i].T, np.dot(retCov * n, weights[:,i]))) for i in range(sims)])
+    pfSR  = pfRet / pfVol
+
+    maxSR = pfSR.max()
+    maxID = pfSR.argmax()
+    maxRet = pfRet[maxID]
+    maxVol = pfVol[maxID]
+    maxWeights = weights[:,maxID]
+
+    return {'pfVol':pfVol,'pfRet':pfRet,'pfSR':pfSR,'weights':weights,'maxSR':maxSR,'maxRet':maxRet,'maxVol':maxVol,'maxWeights':maxWeights}
+
+"""
+TEST:
+mpt = mptPortfolios(1000, returns, assets)
+pfVol = mpt['pfVol']
+pfRet = mpt['pfRet']
+pfSR = mpt['pfSR']
+pfWeights = mpt['weights']
+"""
+
+def mptScatter(pfVol, pfRet, pfSR, weights, returns, n = 12):
+    assets = len(weights[:,0])
+
+    maxID = pfSR.argmax()
+    maxRet = pfRet[maxID]
+    maxVol = pfVol[maxID]
+
+    frontier_y = np.linspace(pfRet.min(),pfRet.max(),100)
+
+    def minimize_volatility(weights, returns, n):
+        return  get_ret_vol_sr(weights, returns, n)[1]
+
+    frontier_volatility = []
+    
+    # 0-1 bounds for each weight
+    bounds = np.array([(0,1) for i in range(assets)])
+
+    # Initial Guess (equal distribution)
+    init_guess = np.repeat(1 / assets, assets)
+
+    for possible_return in frontier_y:
+        # function for return
+        cons = ({'type':'eq','fun': check_sum},
+                {'type':'eq','fun': lambda w: get_ret_vol_sr(w, returns, n)[0] - possible_return})
+        
+        result = minimize(minimize_volatility,init_guess,method='SLSQP',bounds=bounds,constraints=cons, args = (returns, n))
+        
+        frontier_volatility.append(result['fun'])
+
+    plt.figure(figsize=(12,8))
+    plt.scatter(pfVol, pfRet, c = pfSR, cmap = 'plasma')
+    plt.colorbar(label = 'Sharpe Ratio')
+    plt.xlabel('Volatility')
+    plt.ylabel('Return')
+
+    # Add red dot for max SR
+    plt.scatter(maxVol,maxRet,c='red',s=50,edgecolors='black')
+    
+    # Add frontier line
+    plt.plot(frontier_volatility,frontier_y,'g--',linewidth=3)
+    
+    plt.show()
+
+"""
+TEST plot:
+mptScatter(mpt['pfVol'], mpt['pfRet'],mpt['pfSR'],mpt['weights'],returns, n = 12)
+"""
